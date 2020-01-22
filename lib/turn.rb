39f9578025ff9c01e4ad72b59c8ad13548471506
Maharaj Syed <maharajsyed@yahoo.com>
2020-01-22 23:15:10 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,7 +1,6 @@
 #display board:
 
     def display_board(board)
-      puts "Welcome to tic tac toe!"
       puts " #{board[0]} | #{board[1]} | #{board[2]} "
       puts "-----------"
       puts " #{board[3]} | #{board[4]} | #{board[5]} "
